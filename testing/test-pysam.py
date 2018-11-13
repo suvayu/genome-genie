b125457e8bce31e933c7dd97df47b8c31029fe3b
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2018-11-13 15:34:48 +0530

    Update conversion script to work with multiprocessing and asyncio

--- testing/test-pysam.py
+++ testing/test-pysam.py
@@ -2,44 +2,76 @@
 
 import os.path
 import time
+from concurrent.futures import ProcessPoolExecutor
+import asyncio
+import gc
 
 import pyarrow as pa
 import pyarrow.parquet as pq
 
+# from dask.distributed import Client
+# import dask.bag as db
+# import dask.delayed as delay
+
 from pysam import VariantFile
 
 from genomegenie.schemas import get_vcf_cols
-from genomegenie.io import get_header, to_arrow
+from genomegenie.io import get_header, to_arrow, to_parquet
+
+# client = Client("127.0.0.1:8786")
 
 # data files: {20..22},X,Y
-data_t = 'data/ALL.chr{}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
-data_x = 'data/ALL.chrX.phase3_shapeit2_mvncall_integrated_v1b.20130502.genotypes.vcf.gz'
-data_y = 'data/ALL.chrY.phase3_integrated_v1b.20130502.genotypes.vcf.gz'
+data_t = (
+    "data/ALL.chr{}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz"
+)
+data_x = (
+    "data/ALL.chrX.phase3_shapeit2_mvncall_integrated_v1b.20130502.genotypes.vcf.gz"
+)
+data_y = "data/ALL.chrY.phase3_integrated_v1b.20130502.genotypes.vcf.gz"
 
 chrom = 20
-vf = VariantFile(data_t.format(chrom), mode='r', threads=4)
+vfname = data_t.format(chrom)
+vf = VariantFile(vfname, mode="r", threads=4)
 hdr, samples = get_header(vf)
-
-# if not os.path.exists('data/pq/header.pq'):
-#     hdr.to_parquet('data/pq/header.pq')
-
-# # data
-# data_t = pa.union([
-#     pa.field('i', pa.int32()),
-#     pa.field('f', pa.float32())
-# ], 'dense')
+vf.close()
+# hdr.to_parquet("data/pq/chr20-header.parquet")
 
 cols = get_vcf_cols(hdr)
 schema = pa.schema(cols)
 struct = pa.struct(cols)
 
-# # debug:
-# vf_itr = vf.fetch(f"{chrom}", 0, 70_000, reopen=True)
-# for i, r in enumerate(vf_itr):
-#     if i > 102: break           # go to 64139: ALT: A,T
+# run in an asyncio event loop, have better control.
+# wait for batches of 4, append to parquet file
+loop = asyncio.get_event_loop()
+batchsize = 4  # ncores/2
+tproc, tio = 0, 0  # timing
+pqwriter = pq.ParquetWriter("test.parquet", schema, flavor="spark")
+for i in range(0, 64, batchsize):
+    with ProcessPoolExecutor(4) as executor:
+        t0 = time.process_time()
+        tasks = [
+            loop.run_in_executor(
+                executor,
+                to_arrow,
+                vfname,
+                ("20", j * 1_000_000, (j + 1) * 1_000_000),
+                cols,
+            )
+            for j in range(i, i + batchsize)
+        ]
+        res = asyncio.gather(*tasks)  # future that gathers all results
+        loop.run_until_complete(res)  # ensure future/wait
+        t1 = time.process_time()
+        res = res.result()  # get results
+        res = [i for i in res if i.num_rows > 0]
+        if len(res):  # append to file
+            to_parquet(pqwriter, res)
+        t2 = time.process_time()
+        del res  # clean-up memory
+        gc.collect()
+    tproc += t1 - t0
+    tio += t2 - t1
+loop.close()
+pqwriter.close()
 
-t0 = time.process_time()
-batches = [to_arrow(vf.fetch("20", i*500_000, (i + 1)*500_000, reopen=True), cols)
-           for i in range(32)]
-t1 = time.process_time()
-tbl = pa.Table.from_batches(batches, schema)
+print(f"VCF => Parquet: {tproc:.3f}s CPU, {tio:.3f}s I/O")
