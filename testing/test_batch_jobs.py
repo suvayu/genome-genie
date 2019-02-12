ed26ace18d12d23db6af03a8f55dcdbaf83bbec4
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2019-02-12 18:47:07 +0530

    testing/test_batch_jobs: test for staging the pipeline graph

--- testing/test_batch_jobs.py
+++ testing/test_batch_jobs.py
@@ -1,45 +1,58 @@
-from pprint import pprint
+from time import sleep
+from datetime import datetime
+from itertools import product
+from warnings import filterwarnings
 
+# import pytest
+import numpy as np
+import dask
 from dask.distributed import Client
 
-from glom import glom, Coalesce, Inspect
-
-from genomegenie.utils import flatten
-from genomegenie.batch.debug import DummyCluster
 from genomegenie.batch.jobs import Pipeline, results
-from genomegenie.batch.factory import compile_template
+
+
+
+def test_pipeline_stage():
+    graph = [(["foo", "bar"], "baz"), "whaat"]
+
+    def process(key):
+        return [f"{key}-{i}" for i in range(2)] if key.startswith("b") else [f"{key}"]
+
+    @dask.delayed
+    def submit(job, monitor_t, *args):
+        res = {"name": job, "beg": datetime.now()}
+        sleep(3*np.random.rand())
+        res.update(end=datetime.now())
+        return res
+
+    client = Client()
+    staged = Pipeline.stage(graph, process, submit)
+    df = results(staged.compute(), 4, ["name", "beg", "end"])
+
+    foo = df.name.str.startswith("foo")
+    bar = df.name.str.startswith("bar")
+    baz = df.name.str.startswith("baz")
+    wha = df.name.str.startswith("wha")
+
+    # foo ends before bar starts
+    t1 = df[foo].end
+    t2 = df[bar].beg
+    assert max(t1) < min(t2)
+
+    # baz starts before foo or bar ends
+    t1 = df[baz].beg
+    t2 = df[foo | bar].end
+    assert all(i > j for i, j in product(t2, t1))
+
+    # whaat starts after the last of foo, bar, and baz ends
+    t1 = df[foo | bar | baz].end
+    t2 = df[wha].beg
+    assert max(t1) < min(t2)
+
 
 opts = {
-    "pipeline": ["pon_sample", "pon_consolidate", "gatk"],
-    "module": ["gatk-4.0.1", "samtools"],
-    "inputs": [
-        {"normal_bam": "normal1.bam", "tumor_bam": "tumor1.bam"},
-        {"normal_bam": "normal2.bam", "tumor_bam": "tumor2.bam"},
-        {"normal_bam": "normal3.bam", "tumor_bam": "tumor3.bam"},
-    ],
-    "gatk": {
-        "ref_fasta": "reference.fasta",
-        "output": "result.vcf.gz",
-        "db": "somedb.vcf",
-        "pon": "pon.vcf",
-        "nprocs": 4,
-    },
-    "pon_sample": {"ref_fasta": "reference.fasta", "db": "somedb.vcf"},
-    "pon_consolidate": {"inputs": "all", "normals_list": "foo.txt", "pon": "pon.vcf"},
-    "sge": {
-        "name": "faketest",
-        "queue": "short.q",
-        "log_directory": "batch",
-        "walltime": "00:30:00",
-        "cputime": "00:30:00",
-        "memory": "16 GB",
-    },
-}
-
-dummyopts = {
-    # "pipeline": ["prep1", "ajob", "finalize"],
     "pipeline": [(["prep1", "ajob"], "bjob"), "finalize"],
-    "module": ["gatk-4.0.1", "samtools"],
+    "module": ["pkg1", "pkg2"],
     "inputs": [
         {"normal_bam": "normal1.bam", "tumor_bam": "tumor1.bam"},
         {"normal_bam": "normal2.bam", "tumor_bam": "tumor2.bam"},
@@ -73,14 +86,9 @@ dummyopts = {
     },
 }
 
+# if __name__ == '__main__':
+#     cluster = DummyCluster(processes=True)
+#     client = Client(cluster)
+#     res = test_pipeline_stage()
 
-if __name__ == '__main__':
-    cluster = DummyCluster(processes=True)
-    client = Client(cluster)
-    pipeline = Pipeline(cluster, dummyopts)
-    staged = pipeline.stage(pipeline.graph, monitor_t=0)
-    res = staged.compute()
 
-    df = results(res)
-    df.info()
-    df.to_csv('job_output.csv')
