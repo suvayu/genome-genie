a45c389dcf0a84e12178a75bdc3320d5c2b94972
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2019-02-22 15:18:55 +0530

    pipeline-job: configure logger correctly

--- bin/pipeline-job.py
+++ bin/pipeline-job.py
@@ -75,12 +75,19 @@ opts = {
     },
 }
 
-logger = logging.getLogger()
+logger = logging.getLogger("genomegenie")
+handler = logging.StreamHandler()
+fmt = "{levelname}:{asctime}:{name}:{lineno}: {message}"
+formatter = logging.Formatter(fmt, style="{")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 if __name__ == "__main__":
+    logger.setLevel(logging.DEBUG)
+
     cluster = LocalCluster(n_workers=4, processes=True)
     client = Client(cluster)
-    logger.setLevel(logging.DEBUG)
+
     pipeline = Pipeline(opts)
     staged = pipeline.stage(
         pipeline.graph, pipeline.process, pipeline.submit, monitor_t=3600
